849a3eaf8671dcc2c38e592a414bbdbe3ba84529
ShreyanshChougule <shreyansh.chougule@yahoo.com>
2023-10-03 15:25:42 +0530

    Updating New Envoirment in Conftest for browser invoking.

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,5 +1,11 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
 from utilities.Read_Config_File import Read_Confi_File
 
 
@@ -16,13 +22,18 @@ def browser(request):
 @pytest.fixture()
 def setup(browser):
     if browser == 'chrome':
-        driver = webdriver.Chrome()
+        # driver = webdriver.Chrome()
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
     elif browser == 'edge':
-        driver = webdriver.Edge()
+        # driver = webdriver.Edge()
+        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+
     else:
         Chrome_options = webdriver.ChromeOptions()
         Chrome_options.add_argument("headless")
-        driver = webdriver.Edge(options=Chrome_options)
+        # driver = webdriver.Chrome(options=Chrome_options)
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=Chrome_options)
 
     # driver.get("https://admin-demo.nopcommerce.com/login")
     R = Read_Confi_File()
